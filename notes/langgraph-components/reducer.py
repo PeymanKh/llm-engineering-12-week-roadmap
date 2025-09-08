7b31cb59de0cbcdf03a0489c24236562e61e2fea
Peyman Kh <peymankhodabandehlouei@gmail.com>
2025-09-08 10:31:13 +0300

    docs: update langgraph notes

--- notes/langgraph-components/reducer.py
+++ notes/langgraph-components/reducer.py
@@ -9,6 +9,7 @@ With a reducer, we can define accumulation, merging, or more complex logic.
 When we define the state, we can Annotate the reducer function we want.
 """
 # Import libraries
+import operator
 from typing import TypedDict
 from typing_extensions import Annotated
 from langchain_core.messages import AnyMessage
@@ -19,6 +20,11 @@ class State(TypedDict):
     messages: Annotated[list[AnyMessage], add_messages]
     score: Annotated[int, lambda prev, new: prev + new]
 
+"""
+Also we can use builtin operator library to create Annotated fields in our graph state
+"""
+class CustomState(TypedDict):
+    foo: Annotated[list[int], operator.add]
 
 """
 In LangGraph, there exists a prebuilt state called `MessageState` which makes it easy to use messages.
@@ -29,3 +35,28 @@ from langgraph.graph import MessagesState
 class GraphState(MessagesState):
     documents: list[str]
 
+
+
+"""
+Custom Reducers
+"""
+def reduce_list(left: list | None, right: list | None) -> list:
+    """Safely combine two lists, handling cases where either or both inputs might be None.
+
+    Args:
+        left (list | None): The first list to combine, or None.
+        right (list | None): The second list to combine, or None.
+
+    Returns:
+        list: A new list containing all elements from both input lists.
+               If an input is None, it's treated as an empty list.
+    """
+    if not left:
+        left = []
+    if not right:
+        right = []
+    return left + right
+
+
+class CustomReducerState(TypedDict):
+    foo: Annotated[list[int], reduce_list]
--- notes/langgraph-components/state_management.py
+++ notes/langgraph-components/state_management.py
@@ -68,3 +68,79 @@ try:
     state5 = PydanticState(name="Peyman", age=20, gender="meaw")
 except ValidationError as e:
     print("Validation Error:", e)
+
+
+"""
+Multiple Schemas:
+
+In some cases. out nodes might communicate internally, and those schemas doesn't need to be included
+in our main schema which contains input and output of the graph. In such case, we can use multiple
+schemas.
+"""
+from typing_extensions import TypedDict
+from langgraph.graph import StateGraph, START, END
+
+class OverallState(TypedDict):
+    foo: int
+
+class PrivateState(TypedDict):
+    baz: int
+
+def node_1(state: OverallState) -> PrivateState:
+    print("---Node 1---")
+    return {"baz": state['foo'] + 1}
+
+def node_2(state: PrivateState) -> OverallState:
+    print("---Node 2---")
+    return {"foo": state['baz'] + 1}
+
+# Build graph
+builder = StateGraph(OverallState)
+builder.add_node("node_1", node_1)
+builder.add_node("node_2", node_2)
+
+# Logic
+builder.add_edge(START, "node_1")
+builder.add_edge("node_1", "node_2")
+builder.add_edge("node_2", END)
+
+# Add
+graph = builder.compile()
+
+
+"""
+We can also define different states for input and output as follow:
+"""
+class InputState(TypedDict):
+    question: str
+
+
+class OutputState(TypedDict):
+    answer: str
+
+
+class OverallState(TypedDict):
+    question: str
+    answer: str
+    notes: str
+
+
+def thinking_node(state: InputState):
+    return {"answer": "bye", "notes": "... his is name is Lance"}
+
+
+def answer_node(state: OverallState) -> OutputState:
+    return {"answer": "bye Lance"}
+
+
+graph1 = StateGraph(OverallState, input_schema=InputState, output_schema=OutputState)
+graph1.add_node("answer_node", answer_node)
+graph1.add_node("thinking_node", thinking_node)
+graph1.add_edge(START, "thinking_node")
+graph1.add_edge("thinking_node", "answer_node")
+graph1.add_edge("answer_node", END)
+
+graph3 = graph1.compile()
+
+graph.invoke({"question":"hi"})
+
